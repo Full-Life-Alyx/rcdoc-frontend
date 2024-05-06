5ba1266aa73138f8179f2ad150095a0eceb922d7
Full-Life-Alyx <96643025+Full-Life-Alyx@users.noreply.github.com>
2024-05-05 21:42:39 -1000

    Add connection thing for testing backend

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const api_loc = import.meta.env.VITE_API_LOC ?? "http://localhost:80";
+
---
+++ src/lib/Login.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { api_loc } from "../constants";
+
+  let res: string = "whomp";
+  async function send_message() {
+    const url = `${api_loc}/ping`;
+    console.log(url);
+    const responce = await fetch(url);
+    res = await responce.text();
+  }
+</script>
+
+<button on:click={send_message}>{res}</button>
